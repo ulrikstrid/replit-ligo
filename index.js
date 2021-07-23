598b867f1198b99d23c58aa7a5356d09018d423f
Ulrik Strid <ulrikstrid@users.noreply.github.com>
2021-07-23 06:39:27 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log(`Hello from Node.js ${process.version}!`);
+
+// hi, can you see this?
+
+
+We need to add a  contract here
+
+// i told you, take edu's sidechain contracts, he also has tests
---
+++ replit.nix
@@ -0,0 +1,22 @@
+{ pkgs }:
+
+let
+  pkgs = import (builtins.fetchTarball {
+    # Descriptive name to make the store path easier to identify
+    name = "nixos-unstable-2021-07-19";
+    # Commit hash for nixos-unstable as of 2021-07-19
+    url = "https://github.com/nixos/nixpkgs/archive/b59c06dc92f8d03660eb4155754d93a6c34cda83.tar.gz";
+    # Hash obtained using `nix-prefetch-url --unpack <url>`
+    sha256 = "1mjvaasd1f2x0zly3vaj94dgjsqbqii9rdys12pzr2yz7qqrwsvk";
+  }) {};
+  ocamlVersion = "4_10";
+  tezosPkgs = pkgs.callPackage ./nix {
+    ocamlPackages = (pkgs.ocaml-ng."ocamlPackages_${ocamlVersion}");
+  };
+in
+
+{
+  deps = [
+    tezosPkgs.ligo
+  ];
+}
