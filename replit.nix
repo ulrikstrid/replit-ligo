6ece06d7aee44be22de3c21bf1705c71e0dccded
Ulrik Strid <ulrik.strid@outlook.com>
2021-10-12 15:05:10 +0000

    Update to latest ligo

--- replit.nix
+++ replit.nix
@@ -6,9 +6,9 @@ let
       # Descriptive name to make the store path easier to identify
       name = "nixpkgs-ligo-dev";
       # Commit hash for nixos-unstable as of 2021-07-19
-      url = "https://github.com/ulrikstrid/nixpkgs/archive/4e20a7c4fd22d7ad814185117fb3ddd5d4708a9e.tar.gz";
+      url = "https://github.com/ulrikstrid/nixpkgs/archive/cd0276d5333009b641f2e87ac332cbd0338eac2a.tar.gz";
       # Hash obtained using `nix-prefetch-url --unpack <url>`
-      sha256 = "1aac5fp8jrkw23ncwyjppxh28pww83v6ahf9z16y686vl9jp99d9";
+      sha256 = "1z4djc5mb1qavvvnhj5w6kbbg3x34vpi81nqjpnvn2nn4crmml3f";
     })
     { };
 in
